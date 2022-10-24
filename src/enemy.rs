ccdaad12e23811c0f21b320faf5dfdc78762e063
InmoJl <2698513236@qq.com>
2022-10-24 23:23:41 +0800

    update

--- src/enemy.rs
+++ src/enemy.rs
@@ -1,22 +1,34 @@
 use bevy::prelude::*;
+use bevy::time::FixedTimestep;
 use rand::{Rng, thread_rng};
 
-use crate::{ENEMY_SIZE, GameTextures, SPRITE_SCALE, WinSize};
+use crate::{ENEMY_LASER_SIZE, ENEMY_MAX, ENEMY_SIZE, EnemyCount, FromEnemy, GameTextures, Laser, Movable, SPRITE_SCALE, Velocity, WinSize};
 use crate::components::{Enemy, SpriteSize};
 
 pub struct EnemyPlugin;
 
 impl Plugin for EnemyPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system_to_stage(StartupStage::PostStartup, enemy_spawn_system);
+        app.add_system_set(
+            SystemSet::new()
+                .with_run_criteria(FixedTimestep::step(1.))
+                .with_system(enemy_spawn_system)
+        )
+            .add_system(enemy_fire_system);
     }
 }
 
 fn enemy_spawn_system(
     mut commands: Commands,
+    mut enemy_count: ResMut<EnemyCount>,
     game_textures: Res<GameTextures>,
     win_size: Res<WinSize>
 ) {
+
+    if enemy_count.0 >= ENEMY_MAX {
+        return;
+    }
+
     // compute x/y
     let mut rng = thread_rng();
     let w_span = win_size.w / 2. - 100.;
@@ -35,6 +47,33 @@ fn enemy_spawn_system(
     })
         .insert(Enemy)
         .insert(SpriteSize::from(ENEMY_SIZE));
+
+    enemy_count.0 += 1;
 }
 
+fn enemy_fire_system(
+    mut commands: Commands,
+    game_textures: Res<GameTextures>,
+    enemy_query: Query<&Transform, With<Enemy>>
+) {
+    for &tf in enemy_query.iter() {
 
+        let (x, y) = (tf.translation.x, tf.translation.y);
+
+        // spawn enemy laser sprite
+        commands.spawn_bundle(SpriteBundle {
+            texture: game_textures.enemy_laser.clone(),
+            transform: Transform {
+                translation: Vec3::new(x, y - 15., 0.),
+                scale: Vec3::new(SPRITE_SCALE, SPRITE_SCALE, 1.),
+                ..default()
+            },
+            ..default()
+        })
+            .insert(Laser)
+            .insert(SpriteSize::from(ENEMY_LASER_SIZE))
+            .insert(FromEnemy)
+            .insert(Movable { auto_despawn: true })
+            .insert(Velocity { x: 0., y: -1. });
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,8 @@ use bevy::prelude::*;
 use bevy::sprite::collide_aabb::collide;
 use bevy::window::close_on_esc;
 use bevy::math::Vec3Swizzles;
-use crate::components::{Enemy, ExplosionToSpawn, FromPlayer, Laser, Movable, Player, SpriteSize, Velocity};
+use bevy::utils::HashSet;
+use crate::components::*;
 use crate::enemy::EnemyPlugin;
 use crate::player::PlayerPlugin;
 
@@ -19,6 +20,7 @@ const ENEMY_SPRITE: &str = "enemy_a_01.png";
 const ENEMY_LASER_SPRITE: &str = "laser_b_01.png";
 
 const EXPLOSION_SHEET: &str = "explo_a_sheet.png";
+const EXPLOSION_LEN: usize = 16;
 
 // endregion
 
@@ -38,7 +40,7 @@ const SPRITE_SCALE: f32 = 0.5;
 
 pub struct WinSize {
     pub w: f32,
-    pub h: f32
+    pub h: f32,
 }
 
 pub struct GameTextures {
@@ -49,12 +51,15 @@ pub struct GameTextures {
     explosion: Handle<TextureAtlas>, // 存储 grid 资源的一种方式
 }
 
+pub struct EnemyCount(u32);
+
 // endregion
 
 // region    --- Game Constants
 
 const TIME_STEP: f32 = 1. / 60.;
 const BASE_SPEED: f32 = 500.;
+const ENEMY_MAX: u32 = 2;
 
 // endregion --- Game Constants
 
@@ -74,6 +79,8 @@ fn main() {
         .add_startup_system(setup_system)
         .add_system(movable_system)
         .add_system(player_laser_hit_enemy_system)
+        .add_system(explosion_to_spawn_system)
+        .add_system(explosion_animation_system)
         .run();
 }
 
@@ -81,7 +88,7 @@ fn setup_system(
     mut commands: Commands,
     assets_server: Res<AssetServer>,
     mut texture_atlases: ResMut<Assets<TextureAtlas>>,
-    mut windows: ResMut<Windows>
+    mut windows: ResMut<Windows>,
 ) {
 
     // camera
@@ -101,7 +108,7 @@ fn setup_system(
         texture_handle,
         Vec2::new(64., 64.),
         4,
-        4
+        4,
     );
     let explosion = texture_atlases.add(texture_atlas);
 
@@ -111,16 +118,17 @@ fn setup_system(
         player_laser: assets_server.load(PLAYER_LASER_SPRITE),
         enemy: assets_server.load(ENEMY_SPRITE),
         enemy_laser: assets_server.load(ENEMY_LASER_SPRITE),
-        explosion
+        explosion,
     };
-    commands.insert_resource(game_textures);
 
+    commands.insert_resource(game_textures);
+    commands.insert_resource(EnemyCount(0));
 }
 
 fn movable_system(
     mut commands: Commands,
     win_size: Res<WinSize>,
-    mut query: Query<(Entity, &Velocity, &mut Transform, &Movable)>
+    mut query: Query<(Entity, &Velocity, &mut Transform, &Movable)>,
 ) {
     for (entity, velocity, mut transform, movable) in query.iter_mut() {
         let translation = &mut transform.translation;
@@ -143,15 +151,32 @@ fn movable_system(
 
 fn player_laser_hit_enemy_system(
     mut commands: Commands,
+    mut enemy_count: ResMut<EnemyCount>,
     laser_query: Query<(Entity, &Transform, &SpriteSize), (With<Laser>, With<FromPlayer>)>,
-    enemy_query: Query<(Entity, &Transform, &SpriteSize), With<Enemy>>
+    enemy_query: Query<(Entity, &Transform, &SpriteSize), With<Enemy>>,
 ) {
+
+    // 防止同时多个删除同一个实例
+    let mut despawned_entities: HashSet<Entity> = HashSet::new();
+
     // iterate through the lasers
     for (laser_entity, laser_tf, laser_size) in laser_query.iter() {
+
+        if despawned_entities.contains(&laser_entity) {
+            continue;
+        }
+
         let laser_scale = Vec2::from(laser_tf.scale.xy());
 
         // iterate through the enemies
         for (enemy_entity, enemy_tf, enemy_size) in enemy_query.iter() {
+
+            if despawned_entities.contains(&enemy_entity) ||
+                despawned_entities.contains(&laser_entity)
+            {
+                continue;
+            }
+
             let enemy_scale = Vec2::from(enemy_tf.scale.xy());
 
             // determin if collision
@@ -160,16 +185,19 @@ fn player_laser_hit_enemy_system(
                 laser_tf.translation,
                 laser_size.0 * laser_scale,
                 enemy_tf.translation,
-                enemy_size.0 * enemy_scale
+                enemy_size.0 * enemy_scale,
             );
 
             // perform collision 执行碰撞
             if let Some(_) = collision {
                 // remove the enemy
                 commands.entity(enemy_entity).despawn();
+                despawned_entities.insert(enemy_entity);
+                enemy_count.0 -= 1;
 
                 // remove the laser
                 commands.entity(laser_entity).despawn();
+                despawned_entities.insert(laser_entity);
 
                 // spawn the explosionToSpawn
                 commands.spawn().insert(ExplosionToSpawn(
@@ -183,13 +211,40 @@ fn player_laser_hit_enemy_system(
 fn explosion_to_spawn_system(
     mut commands: Commands,
     game_textures: Res<GameTextures>,
-    query: Query<(Entity, &ExplosionToSpawn)>
+    query: Query<(Entity, &ExplosionToSpawn)>,
 ) {
-    for (explosion_spawn_entity, explosion_to_spwan) in query.iter() {
+    for (explosion_spawn_entity, explosion_to_spawn) in query.iter() {
         // spawn the explosion sprite
-         commands.spawn_bundle(SpriteSheetBundle {
-             texture_atlas: game_textures.explosion.clone(),
-             ..default()
-         });
+        commands.spawn_bundle(SpriteSheetBundle {
+            texture_atlas: game_textures.explosion.clone(),
+            transform: Transform {
+                translation: explosion_to_spawn.0,
+                ..default()
+            },
+            ..default()
+        })
+            .insert(Explosion)
+            .insert(ExplosionTimer::default());
+
+        // despawn the explosionToSpawn
+        commands.entity(explosion_spawn_entity).despawn();
+    }
+}
+
+fn explosion_animation_system(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut query: Query<(Entity, &mut ExplosionTimer, &mut TextureAtlasSprite), With<Explosion>>,
+) {
+    for (entity, mut timer, mut sprite) in query.iter_mut() {
+        // 递进时间
+        timer.0.tick(time.delta());
+        // 检查递进后，是否到达指定时间
+        if timer.0.finished() {
+            sprite.index += 1;
+            if sprite.index >= EXPLOSION_LEN {
+                commands.entity(entity).despawn();
+            }
+        }
     }
 }
